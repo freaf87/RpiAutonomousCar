7f621c7ac4f4bd7daad2a68cc87a1cb7aa5edda5
Frederic Afadjigla <freaf87@yahoo.fr>
2017-04-30 20:43:09 +0200

    initial commit

---
+++ 01_Hardware/Cleanup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# Cleanup eagle auto generated files
+import os, glob
+
+try:
+    currentdir = os.path.realpath(__file__)
+    os.chdir(os.path.dirname(currentdir))
+
+    deleteditems = []
+    directories = ["."]
+	
+    for dir in directories:
+        if os.path.isdir(dir):
+            os.chdir(dir)
+            for file in glob.glob("*.s#*"):
+                os.remove(file)
+                deleteditems.append(file)
+            for file in glob.glob("*.b#*"):
+                os.remove(file)
+                deleteditems.append(file)
+            for file in glob.glob("*.l#*"):
+                os.remove(file)
+                deleteditems.append(file)
+            os.chdir(os.path.dirname(currentdir))
+
+    if deleteditems:
+        print("Deleted files:")
+        for item in deleteditems:
+            print(item)
+    else:
+        print("Nothing to be done !!")
+
+    os.system('pause')
+except ValueError:
+    print ValueError
