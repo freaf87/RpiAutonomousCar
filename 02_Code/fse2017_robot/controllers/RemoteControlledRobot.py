f8dad974ec3461866a72b275f02f19cc62a86bb4
Daniel Lee <erget2005@gmail.com>
2017-11-11 18:32:28 +0100

    Refactoring

--- 02_Code/fse2017_robot/controllers/RemoteControlledRobot.py
+++ 02_Code/fse2017_robot/controllers/RemoteControlledRobot.py
@@ -15,131 +15,82 @@
 # You should have received a copy of the GNU General Public License
 # along with FSE 2017.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Server to run on a robot which is remotely controlled by network."""
+
 import socket
-import time
+from logging import debug
 from threading import Thread
 
-from ..drivers.led import LED
-from ..drivers.motor_driver import MotorDriver
-from ..drivers.ultrasonic_ranger import UltrasonicRanger
+from ..robot import Robot
 
-VERBOSE = False
 IP_PORT = 22000
 
 
-# TODO: These are not constants, they're used in the script, so move it down
-# to the main.
-# TODO: These looks like classes. PEP8.
-UltrasonicObj = UltrasonicRanger()
-LedObj = LED()
-RobotDriveObj = MotorDriver()
-
-# TODO: Docstrings!
 class SocketHandler(Thread):
-    def __init__(self, conn):
+    """Server to handle incoming commands from remote control agent."""
+
+    command_terminator = "\0"
+
+    def __init__(self, conn, robot):
         Thread.__init__(self)
         self.conn = conn
+        self.connected = False
+        self.robot = robot
 
     def run(self):
-        # TODO: No globals.
-        global isConnected
         debug("SocketHandler started")
         while True:
             cmd = ""
             try:
-                debug("Calling blocking conn.recv()")
                 cmd = self.conn.recv(1024)
-            except:
-                debug("exception in conn.recv()") 
-                # happens when connection is reset from the peer
+            except socket.error:
+                debug("Connection was reset by peer.")
                 break
             debug("Received cmd: " + cmd + " len: " + str(len(cmd)))
-            if len(cmd) == 0:
+            if not len(cmd):
                 break
-            self.executeCommand(cmd)
-        conn.close()
-        print "Client disconnected. Waiting for next client..."
-        isConnected = False
+            self.execute(cmd)
+        connection.close()
+        print("Client disconnected. Waiting for next client...")
+        self.connected = False
         debug("SocketHandler terminated")
 
-    def executeCommand(self, cmd):
-        # TODO: No globals!
-        global RobotDrive
+    def execute(self, cmd):
+        print("Reporting current state: {}".format(cmd))
+        self.conn.sendall(cmd)
+        self.conn.sendall(cmd)
 
-        #debug("Calling executeCommand() with  cmd: " + cmd)
-        state = cmd[:-1]# remove trailing "\0"
-        print "Reporting current state:", state
-        self.conn.sendall(state + "\0")
-        self.conn.sendall(state + "\0")
-
-        # TODO: This is modified later to use new API
-        if state == "forward":
-            RobotDriveObj.forward()
-        elif state == "reverse":
-            RobotDriveObj.reverse()
-        elif state == "left":
-            RobotDriveObj.left()
-        elif state == "right":
-            RobotDriveObj.right()
-        elif state == "stop":
-            RobotDriveObj.stop()
+        if cmd == "forward":
+            self.robot.motor.forward()
+        elif cmd == "reverse":
+            self.robot.motor.reverse()
+        elif cmd == "left":
+            self.robot.motor.left()
+        elif cmd == "right":
+            self.robot.motor.right()
+        elif cmd == "stop":
+            self.robot.motor.stop()
         else:
-            RobotDriveObj.stop()
-
-# TODO: What's the point?
-def setup():
-    pass
-
-# TODO: Replace with debug from logging, also mentioned elsewhere
-def debug(text):
-    if VERBOSE:
-        print "Debug:---", text
-
-def loop():
-    # TODO: PEP8
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # close port when process exits:
-    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    debug("Socket created")
-    HOSTNAME = "" # Symbolic name meaning all available interfaces
-
-    try:
-        serverSocket.bind((HOSTNAME, IP_PORT))
-    except socket.error as msg:
-        # TODO: Raise an exception, don't sys.exit(). You never know who'll
-        # import this code and sys.exit kills the whole process.
-        print "Bind failed", msg[0], msg[1]
-        sys.exit()
-    serverSocket.listen(10)
-
-    print "Waiting for a connecting client..."
-    isConnected = False
-
-    while True:
-        debug("Calling blocking accept()...")
-        conn, addr = serverSocket.accept()
-        print "Connected with client at " + addr[0]
-        isConnected = True
-        socketHandler = SocketHandler(conn)
-        # necessary to terminate it at program termination:
-        socketHandler.setDaemon(True)
-        socketHandler.start()
-        t = 0
-        # TODO: This never terminates. What's the point of isConnected?
-        while isConnected:
-            print "Server connected at", t, "s"
-            time.sleep(10)
-            t += 10
-
+            self.robot.motor.stop()
 
 
 if __name__ == "__main__":
+    with Robot() as r:
+        try:
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # Close port when process exits
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            debug("Socket created")
+            HOSTNAME = ""  # Symbolic name for all available interfaces
+            server_socket.bind((HOSTNAME, IP_PORT))
+            server_socket.listen(10)
+            print("Waiting for a connecting client...")
 
-    try:
-        setup()
-        loop()
-    except KeyboardInterrupt:
-        UltrasonicObj.__exit__()
-        LedObj.__exit__()
-        RobotDriveObj.__exit__()
-
+            while True:
+                debug("Calling blocking accept()...")
+                connection, address = server_socket.accept()
+                print("Connected with client at " + address[0])
+                socketHandler = SocketHandler(connection, r)
+                # Terminate socket handler with parent thread
+                socketHandler.setDaemon(True)
+                socketHandler.start()
